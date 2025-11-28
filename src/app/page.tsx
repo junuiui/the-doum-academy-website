be42e2d5f9d0e9fee7cc4ff5c9769f3fb54f725c
junuiui <jhong980821@outlook.com>
2025-11-27 17:56:36 -0800

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 // src/app/page.tsx
+'use client'
+import { useLanguage } from './context/LanguageContext';
 import styles from './page.module.css';
 
 export default function Home() {
+    const { lang } = useLanguage();
+
     return (
         <div className={styles.container}>
             <section className={styles.hero}>
-                <h1>Welcome to The Doum Academy</h1>
+                {lang === 'en' ? <h1>Welcome to The Doum Academy</h1> : <h1>도움아카데이메 오신것을 환영합니다!</h1> }
                 <p>Brief intro</p>
-            </section>  
+            </section>
 
             {/* brief */}
             <section className={styles.about}>
