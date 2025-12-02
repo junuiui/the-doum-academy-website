4a5aa092dcc1bdcc295f91aa5c2b32dd45ae62f0
junuiui <jhong980821@outlook.com>
2025-12-01 17:22:10 -0800

    form added

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -1,4 +1,32 @@
+import styles from "./page.module.css"
+
 
 export default function BookAppointmentPage() {
-    return <div>book appointment</div>
+    return (
+        <form className={styles.container}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    // value={formData.name}
+                    // onChange={handleChange}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    // value={formData.email}
+                    // onChange={handleChange}
+                    required
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
