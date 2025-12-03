e4cce77c0d5bcaef52746ff2f04e3e40f3b301bf
Junui Hong <jha357@sfu.ca>
2025-12-03 12:57:30 -0800

    gallery placeholder added

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,19 @@
+import styles from './page.module.css'
+
 export default function GalleryPage() {
-    return <div>Gallery</div>
+    return (
+        <main className={styles.container}>
+
+            <div>
+                <h2>Port Moody</h2>
+                pics
+            </div>
+
+            <div>
+                <h2>Vancouver</h2>
+                pics
+            </div>
+
+        </main>
+    )
 }
