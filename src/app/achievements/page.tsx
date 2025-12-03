8366c01eebeb96e8c26326cc0d9b2c78b6754464
Junui Hong <jha357@sfu.ca>
2025-12-03 12:50:05 -0800

    slides placeholder added

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,3 +1,4 @@
+import Slides from '@/components/Slides'
 import styles from './page.module.css'
 
 export default function AchievementPage() {
@@ -5,14 +6,17 @@ export default function AchievementPage() {
         <main className={styles.container}>
             <div>
                 <h2>2025</h2>
+                <Slides />
             </div>
 
             <div>
                 <h2>2024</h2>
+                <Slides />
             </div>
 
             <div>
                 <h2>2023</h2>
+                <Slides />
             </div>
 
         </main>   
