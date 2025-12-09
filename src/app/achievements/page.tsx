c25bc83e3be7697382cd77d9490dde8557a45317
Junui Hong <jha357@sfu.ca>
2025-12-09 14:54:33 -0800

    slide added

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -5,20 +5,32 @@ export default function AchievementPage() {
     return (
         <main className={styles.container}>
             <div>
-                <h2>2025</h2>
-                <Slides />
+                <Slides
+                    year={2025}
+                    images={[
+                        '/achievements/example1.png',
+                        '/acceptance/2025/a2.png',
+                    ]} />
             </div>
 
             <div>
-                <h2>2024</h2>
-                <Slides />
+                <Slides
+                    year={2024}
+                    images={[
+                        '/acceptance/2025/a1.jpg',
+                        '/acceptance/2025/a2.png',
+                    ]} />
             </div>
 
             <div>
-                <h2>2023</h2>
-                <Slides />
+                <Slides
+                    year={2023}
+                    images={[
+                        '/acceptance/2025/a1.jpg',
+                        '/acceptance/2025/a2.png',
+                    ]} />
             </div>
 
-        </main>   
+        </main>
     )
 }
--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -1,9 +1,42 @@
+
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
 import styles from './Slides.module.css'
 
-export default function Slides() {
+type Props = {
+    year: number
+    images: string[]
+}
+
+export default function Slides({ year, images }: Props) {
+    const [index, setIndex] = useState(0)
+
+    const prev = () =>
+        setIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+
+    const next = () =>
+        setIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1))
+
     return (
-        <div>
-            Slide shows...
-        </div>
+        <section className={styles.section}>
+            <h2>{year}</h2>
+
+            <div className={styles.slider}>
+                <button onClick={prev}>&lt;</button>
+
+                <div className={styles.imageWrapper}>
+                    <Image
+                        src={images[index]}
+                        alt={`${year} acceptance letter`}
+                        fill
+                        style={{ objectFit: 'contain' }}
+                    />
+                </div>
+
+                <button onClick={next}>&gt;</button>
+            </div>
+        </section>
     )
 }
