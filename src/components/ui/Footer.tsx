bcd2a91283cb892d225edeb6e47cbd3153e0c873
Junui Hong <jha357@sfu.ca>
2025-12-21 21:10:18 -0800

    instagram link deleted

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import styles from './Footer.module.css';
-import { FaInstagram } from "react-icons/fa6";
 
 export default function Footer() {
-
-    const openInstagram = () => {
-        window.open("https://www.instagram.com/thedoumacademy/", "_blank");
-    };
-
     return (
         <div>
             <footer className={styles.footer}>
