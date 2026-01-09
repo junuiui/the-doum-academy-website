8c5c0e0502821a7e7bc55ee518a4cfee25ac8847
Junui Hong <jha357@sfu.ca>
2026-01-09 13:33:45 -0800

    error test added

--- backend/src/controllers/health.contollers.ts
+++ backend/src/controllers/health.contollers.ts
@@ -6,3 +6,10 @@ export const healthCheck = (req: Request, res: Response) => {
     // send simple status response
     res.json({ status: 'ok' })
 }
+
+// Controller to test error handling
+export const healthErrorTest = (req: Request, res: Response) => {
+    
+    // Intentionally throw an error to test global error handler
+    throw new Error('Health error test')
+}
--- backend/src/routes/health.route.ts
+++ backend/src/routes/health.route.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { healthCheck } from '../controllers/health.contollers'
+import { healthCheck, healthErrorTest } from '../controllers/health.contollers'
 
 const router = Router()
 
 // Health check endpoint
 // /GET /health
 router.get('/', healthCheck)
+
+// Error checking 
+// /GET /health/error
+router.get('/error', healthErrorTest)
 
 export default router
