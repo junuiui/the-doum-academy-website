f12c7752937aa4f268764aa30014e3d95bff916d
Junui Hong <jha357@sfu.ca>
2026-01-09 13:27:53 -0800

    error handler (global) added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+
+// Routes =================
 import healthRouter from './routes/health.route'
+
+// Middleware =================
+import { errorHandler } from './middlewares/error.middleware'
 
 const app: express.Express = express()
 
@@ -9,5 +14,9 @@ app.use(express.json())
 // Register health check route
 // All requests starting with /health will be handled here
 app.use('/health', healthRouter)
+
+
+// Register global error handler
+app.use(errorHandler);
 
 export default app
