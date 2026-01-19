c3cc7b552ae13f6df331915a36e0b153d9efbef0
junuiui <jhong980821@outlook.com>
2026-01-19 13:52:13 -0800

    location router added

--- backend/src/config/routes.config.ts
+++ backend/src/config/routes.config.ts
@@ -4,6 +4,7 @@ import inquiryRouter from "../routes/inquiry.route";
 import teacherRouter from "../routes/teacher.route";
 import achievementRouter from "../routes/achievement.route";
 import serviceRouter from "../routes/service.route";
+import locationRouter from "../routes/location.route";
 
 // register all application routes
 export function addRoutes(app: Express) {
@@ -12,5 +13,5 @@ export function addRoutes(app: Express) {
     app.use('/achievements', achievementRouter);
     app.use('/services', serviceRouter)
     app.use('/inquiries', inquiryRouter);
-
+    app.use('/locations', locationRouter)
 }
