29edfafe837d198f48ae9d0fc1bb276bb3af428f
junuiui <jhong980821@outlook.com>
2026-01-23 15:37:29 -0800

    duplicated schema changed

--- backend/src/models/service.model.ts
+++ backend/src/models/service.model.ts
@@ -1,42 +1,33 @@
 import { Schema, model } from 'mongoose';
 
-const LocalizedTextSchema = new Schema(
-    {
-        en: { type: String, required: true },
-        ko: { type: String, required: true },
-    },
-    { _id: false }
+const LangSchema = new Schema(
+  {
+    en: { type: String, required: true },
+    ko: { type: String, required: true },
+  },
+  { _id: false }
 );
-
-const ServiceBodySchema = new Schema(
-    {
-        en: { type: String, required: true },
-        ko: { type: String, required: true },
-    },
-    { _id: false }
-);
-
 const ServiceSchema = new Schema(
-    {
-        id: {
-            type: Number,
-            required: true,
-            unique: true,
-        },
-
-        title: {
-            type: LocalizedTextSchema,
-            required: true,
-        },
-
-        body: {
-            type: [ServiceBodySchema],
-            required: true,
-        },
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true,
-    }
+
+    title: {
+      type: LangSchema,
+      required: true,
+    },
+
+    body: {
+      type: [LangSchema],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export const Service = model('Service', ServiceSchema);
